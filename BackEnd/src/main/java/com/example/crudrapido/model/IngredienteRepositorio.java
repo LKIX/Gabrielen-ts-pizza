6ca147164487ce3e7f289b1f836929379e2ace7d
FernandL9 <faliendo.23@est.ucab.edu.ve>
2024-11-10 18:14:33 -0400

    Update IngredienteRepositorio.java

---
+++ BackEnd/src/main/java/com/example/crudrapido/model/IngredienteRepositorio.java
@@ -0,0 +1,7 @@
+package com.example.crudrapido.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredienteRepositorio extends JpaRepository<Ingrediente,String>{
+    
+}
